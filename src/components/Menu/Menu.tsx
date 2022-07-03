62c3e05414f9f5c30e3d9826adb5ad28e68e6018
Martin Aleksiev <chochinize@gmail.com>
2022-07-03 19:32:38 +0200

    clear

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -6,6 +6,8 @@ import { bindActionCreators } from 'redux';
 import { actionCreators, State } from '../../state';
 import { JoinMainRoom, ExitMainRoom } from './../../API_Call/apiCall'
 import { useCookies } from 'react-cookie'
+import Room from '../Room/Room';
+import axios from 'axios'
 
 
 
@@ -13,62 +15,37 @@ import { useCookies } from 'react-cookie'
 interface Isocket {
     onopen: (arg: any) => any;
     onmessage: (arg: any) => any
-    close: () => void
+    close: () => void;
+    send:(arg:string)=>any
 }
 
 
 const Menu = () => {
+    const [user, setUser] = useState<IUser>({ name: '' })
+    // let navigate = useNavigate();
+    // const id = Math.floor(Math.random() * Math.floor(Math.random() * Date.now()))
+    
+    // // const [cookies, setCookie, removeCookie] = useCookies(["UID"]);
+    
+    // // function handleCookie(arg:number) {
+    // //   setCookie("UID", arg, {
+        // //     path: "/"
+        // //   });
+        // // }    
+        // // function handleRemoveCookie() {
+    // //     removeCookie("UID");
+    // //   }                   
 
-
-    const [cookies, setCookie, removeCookie] = useCookies(["UID"]);
-
-    function handleCookie(arg:number) {
-      setCookie("UID", arg, {
-        path: "/"
-      });
-    }    
-    function handleRemoveCookie() {
-        removeCookie("UID");
-      }                   
-
-    const effectRan = useRef(false)
-
-    useEffect(() => {
-
-
-        // if(effectRan.current === false){
-        //     const fetchUsers = async () => {
-        //         const response = await fetch('https://jsonplaceholder.typicode.com/users')
-        //         const json = await response.json()
-        //         console.log(json)
-
-
-        //     }
-        //     fetchUsers()
-        //     return ()=>{  
-        //         console.log('unmounted')
-        //         effectRan.current = true
-        //     }
-        // }
-
-
-
-
-    }, [])
-
-    let navigate = useNavigate();
+    // const effectRan = useRef(false)   
     const [client, setClient] = useState<Isocket | null>()
-
     const dispatch = useDispatch()
     const { depositMoney, withDraw, bankrupt, join } = bindActionCreators(actionCreators, dispatch)
     const amount = useSelector((state: State) => state.bank)
-    const id = Math.floor(Math.random() * Math.floor(Math.random() * Date.now()))
+    const[msg,setMsg]= useState<any[]>([])
     const wConnect = () => {
         const frontclient = new ws('ws://localhost:5050'); //  Initialize  the client
-        JoinMainRoom(id)
-        handleCookie(id)
         setClient(frontclient)
-        navigate("/room", { replace: true });
+        // navigate("/room", { replace: true });
         console.log('you are connectet')
     }
     useEffect(() => {
@@ -76,24 +53,82 @@ const Menu = () => {
         client.onopen = () => {
             client.onmessage = (message) => {
                 console.log('what message', message)
+                setMsg(msg=>[...msg,message])
             }
         }
     }, [client])
     const wDisconnect = () => {
         client && client.close()
-        ExitMainRoom(id)
-        removeCookie('UID');
+
         console.log('disconnect')
-        navigate('/', { replace: true })
+        // navigate('/', { replace: true })
         setClient(null)
     }
+    const JoinRoom = () => {
+        setMsg(msg=>[...msg,user.name])
+        client?.send(JSON.stringify({
+            type:'subscribeToChannel',
+            name:user.name,
+        }))
+    };
+    const LeaveRoom = ()=>{
+        client?.send(JSON.stringify({
+            type:'unsubscribeToChannel',
+            name:user.name,
+        }))
+    }
+    
+    
+    // // console.log(client)
+    // // console.log(cookies)
+    interface IUser {
+        name: any;
+    }
+    
+  
+
+    const onChangeInput = (e: any) => {
+        console.log(user)
+        const { name, value } = e.target;
+        setUser({ ...user, [name]: value });
+    };
+    // const JoinRoom = async (e: any) => {
+    //     e.preventDefault();
+    //     try {
+    //         const res = await axios.post("/joinMainRoom", { name: user.name });
+    //     } catch (err) {
+    //         console.log(err)
+    //     }
+    // };
+    console.log(msg.map(i=>i.data))
     return (
-        <div className='w-full h-1/4'>
-            <div className='flex flex-col gap-4 justify-center border-2 h-full '>
-                {cookies.UID}
-                <button onClick={() => wConnect()} className={`${client ? 'hidden' : 'bg-blue-200 mx-24 rounded-md absolute top-2'}`}>Connect</button>
-                <button onClick={() => wDisconnect()} className={`${!client ? 'hidden' : 'bg-red-200 mx-24 rounded-md absolute top-2'}`}>Disconect</button>
-            </div>
+        <div className='border-2   w-max flex text-xs '>
+            <button onClick={() => wConnect()} className={`${client  ? 'hidden' : 'bg-blue-200 mx-24 rounded-md absolute top-2'}`}>Connect</button>
+            <button onClick={() => wDisconnect()} className={`${!client  ?  'hidden' : 'bg-red-200 mx-24 rounded-md absolute top-2'}`}>Disconect</button>
+            <input
+                type="text"
+                name="name"
+                required
+                placeholder="Your Name"
+                className=" absolute top-10 placeholder-shadow-xl outline-none text-center "
+                onChange={onChangeInput}
+            />
+            <button onClick={()=>JoinRoom()}>Join room</button>
+            <button onClick={()=>LeaveRoom()}>Leave room</button>
+            {/* <form onSubmit={JoinRoom}>
+
+                <button onClick={() => wConnect()} className={`${client ? 'hidden' : 'bg-blue-200 mx-2 rounded-md absolute '}`}>Connect</button>
+                <button onClick={() => wDisconnect()} className={`${!client ? 'hidden' : 'bg-red-200 mx-2 rounded-md absolute '}`}>Disconect</button>
+            </form> */}
+            {/* <div className='flex flex-col gap-4 justify-center border-2 h-full '>
+                <label htmlFor="">
+                
+
+                </label>
+                
+            </div> */}
+            {/* {msg.map(i=>i)} */}
+            
         </div>
     )
 }
--- src/components/Room/Room.tsx
+++ src/components/Room/Room.tsx
@@ -2,33 +2,12 @@ import React, { useEffect, useState, useRef } from 'react'
 import { getPlayers } from '../../API_Call/apiCall'
 
 const Room = () => {
+
   const [ player, setPlayer ] = useState<any>([])
   const effectRan = useRef(false)
 
   console.log(player.data?.players[0].users.map((player:any)=><li>{player}</li>))
   useEffect(() => {
-    // if (effectRan.current === false) {
-    //   const fetchUsers = async () => {
-    //     const response = await fetch('http://localhost:5050', {
-    //       method: 'POST',
-    //       headers: { 'Content-Type': 'application/json' },
-    //       credentials: 'include',
-    //       mode: 'no-cors',
-    //       body: JSON.stringify({ title: 'React POST Request Example' })
-
-    //     })
-    //     const json = await response.json()
-    //     console.log(json)
-
-
-    //   }
-    //   fetchUsers()
-    //   return () => {
-    //     console.log('unmounted')
-    //     effectRan.current = true
-    //   }
-    // }
- 
     if(effectRan.current === false){
       const fetchPlayers =  async()=>{
         let players = await getPlayers()
@@ -43,7 +22,8 @@ const Room = () => {
 
   return (
 <div className="flex w-2 flex-wrap overflow ">
-{player.data?.players[0].users.map((player:any, i:any)=><li key={i} className='list-none text-xs mx-4' > user:{player}</li>)}
+  
+{/* {player.data?.players[0].users.map((player:any, i:any)=><li key={i} className='list-none text-xs mx-4' > user:{player}</li>)} */}
  
 </div>
   )
