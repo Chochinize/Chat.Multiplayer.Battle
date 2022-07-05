832bfb99b5fa2e9181f9da6bb9a30a4ee40d9fdc
Martin Aleksiev <chochinize@gmail.com>
2022-07-05 02:33:40 +0200

    salut

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -35,11 +35,17 @@ const Menu = () => {
             }
         }
     }, [client])
-    const randomID = Math.floor(Math.random()*1000)
+    console.log(cookie.UID)
+    useEffect(()=>{
+        if(cookie.UID) {
+            navigate(`/mainroom/${cookie.UID}`)
+        }   
+    },[client])
     const wConnect = () => {
+        const randomID = Math.floor(Math.random()*1000)
         const frontclient = new ws(websocketURL); //  Initialize  the client
         navigate(`/mainroom/${randomID}`, { replace: true })
-        setCookies('UID',randomID);
+        setCookies('UID',randomID,{ path: `/mainroom/${randomID}` });
         join(frontclient)
         setClient(frontclient)
         console.log('you are connectet')
@@ -47,7 +53,12 @@ const Menu = () => {
     }
     return (
         <div className='w-max flex flex-col m-auto gap-5  text-xs '>
-            <button onClick={() => wConnect()} className={`${client ? 'hidden' : 'bg-blue-200 p-2 mx-24 rounded-md m-auto  '}`}>Connect</button>
+            {/* <button onClick={() => wConnect()} className={`${client ? 'hidden' : 'bg-blue-200 p-2 mx-24 rounded-md m-auto  '}`}>Connect</button> */}
+            {cookie.UID 
+            ? 
+            <button onClick={() => wConnect()} className={`${client ? 'hidden' : 'bg-red-200 p-2 mx-24 rounded-md m-auto  '}`}>Reconnect</button> 
+            :
+             <button onClick={() => wConnect()} className={`${client ? 'hidden' : 'bg-blue-200 p-2 mx-24 rounded-md m-auto  '}`}>Connect</button>}
         </div>
     )
 }
--- src/components/Room/MainRoom.tsx
+++ src/components/Room/MainRoom.tsx
@@ -101,7 +101,6 @@ const MainRoom = () => {
           </div>)}
 
 
-
         </div>
       </div>
 
