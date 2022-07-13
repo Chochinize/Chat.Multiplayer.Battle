c3a3903c7ae56bb3ae11cd18cbda2edb43284a13
Martin Aleksiev <chochinize@gmail.com>
2022-07-13 11:30:24 +0200

    types

--- src/components/ChatBox.tsx/showarea/index.tsx
+++ src/components/ChatBox.tsx/showarea/index.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { State } from '../../../state';
 import { useSelector } from 'react-redux'
-
+import moment from 'moment'
 
 
 const ShowArea = () => {
@@ -26,15 +26,15 @@ const ShowArea = () => {
   return (
     <div 
     ref={el}
-    className='border-4 text-[24px]   w-full p-2 h-[100%] overflow-x-auto border-red-400 font-Dongle '>
+    className='border-4 text-[20px]    w-full p-2  h-[100%] overflow-x-auto border-red-400 font-Dongle '>
       
     {client.chatPlayer?.map((item: any, index:number) =>
-    <ul key={index} className='flex items-center gap-[2px] border-2  border-teal-400 w-full break-words  '>
+    <ul key={index} className='flex h-12 mb-2 items-center   border-2  border-teal-400 w-full break-words  '>
       <li className='ml-2 font-semibold'> {item?.name}</li>  
       <li className='font-semibold'> #</li>  
-      
-      <li className='font-norsemiboldmal'> {item?.id} </li> :
-      <li className='ml-2 w-full  border-2 border-semibold'> {item?.chatMessage} </li> 
+      <li className='font-norsemiboldmal mr-2'>{item?.id}</li>
+      <li className='font-norsemiboldmal'> {moment().format('dddd')} </li> :
+      <li className='ml-2 w-max  border-2 border-semibold '> {item?.chatMessage} </li> 
     </ul>
   )}
       
