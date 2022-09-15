a8493130d3e14c699cd0067ee5eb8be598ef8e80
Martin Aleksiev <chochinize@gmail.com>
2022-09-15 12:51:59 +0200

    height fix

--- src/components/ChatBox.tsx/index.tsx
+++ src/components/ChatBox.tsx/index.tsx
@@ -4,7 +4,7 @@ import TextArea from './textarea'
 
 const index = () => {
   return (
-    <div className=' md:w-[35vw] lg:w-[35vw] sm:relative fixed top-[67%] sm:top-[0%] w-[95vw] sm:w-[20vw] xl:w-[20vw] h-[70%] sm:h-full flex  flex-col '>
+    <div className=' md:w-[35vw] lg:w-[35vw] sm:relative fixed top-[70%] sm:top-[0%] w-[95vw] sm:w-[20vw] xl:w-[20vw] h-[70%] sm:h-full flex  flex-col '>
       <ShowArea />
       <TextArea/>
     </div>
